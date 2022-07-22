938a7c2d8abae52e1ed0a2a964d5387fe20268a0
Guillaume Lours <guillaume.lours@docker.com>
2022-07-22 11:22:13 +0200

    bump version of Compose to v2.7.0
    Signed-off-by: Guillaume Lours <guillaume.lours@docker.com>

--- common.mk
+++ common.mk
@@ -40,7 +40,7 @@ REF                ?= HEAD
 DOCKER_CLI_REF     ?= $(REF)
 DOCKER_ENGINE_REF  ?= $(REF)
 DOCKER_SCAN_REF    ?= v0.17.0
-DOCKER_COMPOSE_REF ?= v2.6.1
+DOCKER_COMPOSE_REF ?= v2.7.0
 DOCKER_BUILDX_REF  ?= v0.8.2
 
 # Use "stage" to install dependencies from download-stage.docker.com during the
