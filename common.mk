0d579c4657f745115e703ad04c3e8ed7a34086b5
CrazyMax <crazy-max@users.noreply.github.com>
2023-03-10 09:49:25 +0100

    update buildx to v0.10.4
    full diff: https://github.com/docker/buildx/compare/v0.10.3...v0.10.4
    
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>
    (cherry picked from commit d1ff577bfcf0bf9447220255504c6568b3ae5973)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- common.mk
+++ common.mk
@@ -41,7 +41,7 @@ DOCKER_CLI_REF     ?= $(REF)
 DOCKER_ENGINE_REF  ?= $(REF)
 DOCKER_SCAN_REF    ?= v0.23.0
 DOCKER_COMPOSE_REF ?= v2.16.0
-DOCKER_BUILDX_REF  ?= v0.10.3
+DOCKER_BUILDX_REF  ?= v0.10.4
 
 # Use "stage" to install dependencies from download-stage.docker.com during the
 # verify step. Leave empty or use any other value to install from download.docker.com
