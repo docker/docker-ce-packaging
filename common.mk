6202ccc6574f5014cc55bdb5b165bdd84f874db8
Justin Chadwell <me@jedevc.com>
2023-05-12 16:19:22 +0100

    update buildx to v0.11.0-rc1
    release notes: https://github.com/docker/buildx/releases/tag/v0.11.0-rc1
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- common.mk
+++ common.mk
@@ -39,7 +39,7 @@ REF                ?= HEAD
 DOCKER_CLI_REF     ?= $(REF)
 DOCKER_ENGINE_REF  ?= $(REF)
 DOCKER_COMPOSE_REF ?= v2.17.3
-DOCKER_BUILDX_REF  ?= v0.10.4
+DOCKER_BUILDX_REF  ?= v0.11.0-rc1
 
 # Use "stage" to install dependencies from download-stage.docker.com during the
 # verify step. Leave empty or use any other value to install from download.docker.com
