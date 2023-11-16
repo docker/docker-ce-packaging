7a0d31198cc4c09cbc22d4bae61d5c5ab8b8756b
Tonis Tiigi <tonistiigi@gmail.com>
2023-11-16 15:43:04 -0800

    update buildx to v0.12.0
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- common.mk
+++ common.mk
@@ -39,7 +39,7 @@ REF                ?= HEAD
 DOCKER_CLI_REF     ?= $(REF)
 DOCKER_ENGINE_REF  ?= $(REF)
 DOCKER_COMPOSE_REF ?= v2.23.1
-DOCKER_BUILDX_REF  ?= v0.11.2
+DOCKER_BUILDX_REF  ?= v0.12.0
 
 # Use "stage" to install dependencies from download-stage.docker.com during the
 # verify step. Leave empty or use any other value to install from download.docker.com
