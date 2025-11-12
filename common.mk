ec389254d16c7f309709a3afafcfe53d413fb05b
Jonathan A. Sternberg <jonathan.sternberg@docker.com>
2025-11-12 16:01:58 -0600

    update buildx to v0.30.0
    Signed-off-by: Jonathan A. Sternberg <jonathan.sternberg@docker.com>

--- common.mk
+++ common.mk
@@ -44,7 +44,7 @@ DOCKER_ENGINE_REF  ?= $(REF)
 DOCKER_COMPOSE_REF ?= v2.40.3
 # DOCKER_BUILDX_REF is the version of compose to package. It usually is a tag,
 # but can be a valid git reference in DOCKER_BUILDX_REPO.
-DOCKER_BUILDX_REF  ?= v0.29.1
+DOCKER_BUILDX_REF  ?= v0.30.0
 # DOCKER_MODEL_REF is the version of model to package. It is usually a tag,
 # but can be a valid git reference in DOCKER_MODEL_REPO.
 DOCKER_MODEL_REF   ?= v0.1.39
